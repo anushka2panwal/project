17c3402703717d41bade7b142d1403b980ab058e
anushka2panwal <51848469+anushka2panwal@users.noreply.github.com>
2019-06-15 13:37:10 +0530

    Add files via upload

---
+++ student_extract.bat
@@ -0,0 +1,7 @@
+@ECHO ON
+
+C:\Users\Shankar\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Python.exe C:\Users\Shankar\.PyCharmCE2019.1\config\scratches\student_extract.py
+
+ECHO Attendance exported to details_for_a_student.csv successfully...
+
+PAUSE
